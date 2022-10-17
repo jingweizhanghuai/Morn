d2a3aa1e6dc3f78b6cc316c7e0cfef2a46c511b6
qyli <jingweizhanghuai@163.com>
2022-10-17 08:05:57 +0800

    CRC

--- test/test_CRC.c
+++ test/test_CRC.c
@@ -6,49 +6,49 @@ int main()
     char *data = "hello world";int size=strlen(data);
     char *type;
     
-    type="CRC8";mPropertyWrite("CRC","type",type);
+    type="CRC8";mPropertyWrite("CRC","type",type,32);
     printf("type=%s,crc=0x%x\n",type,mCRC(data,size));
     
-    type="ITU";mPropertyWrite("CRC","type",type);
+    type="ITU";mPropertyWrite("CRC","type",type,32);
     printf("type=%s,crc=0x%x\n",type,mCRC(data));
     
-    type="ROHC";mPropertyWrite("CRC","type",type);
+    type="ROHC";mPropertyWrite("CRC","type",type,32);
+    printf("type=%s,crc=0x%x\n",type,mCRC(data));
+//     
+    type="MAXIM-8";mPropertyWrite("CRC","type",type,32);
+    printf("type=%s,crc=0x%x\n",type,mCRC(data));
+
+    type="X25";mPropertyWrite("CRC","type",type,32);
     printf("type=%s,crc=0x%x\n",type,mCRC(data));
     
-    type="MAXIM-8";mPropertyWrite("CRC","type",type);
+    type="CCITT";mPropertyWrite("CRC","type",type,32);
     printf("type=%s,crc=0x%x\n",type,mCRC(data));
     
-    type="X25";mPropertyWrite("CRC","type",type);
+    type="CCITT-FALSE";mPropertyWrite("CRC","type",type,32);
     printf("type=%s,crc=0x%x\n",type,mCRC(data));
     
-    type="CCITT";mPropertyWrite("CRC","type",type);
+    type="XMODEM";mPropertyWrite("CRC","type",type,32);
+    printf("type=%s,crc=0x%x\n",type,mCRC(data));
+
+    type="IBM";mPropertyWrite("CRC","type",type,32);
     printf("type=%s,crc=0x%x\n",type,mCRC(data));
     
-    type="CCITT-FALSE";mPropertyWrite("CRC","type",type);
+    type="MAXIM-16";mPropertyWrite("CRC","type",type,32);
     printf("type=%s,crc=0x%x\n",type,mCRC(data));
     
-    type="XMODEM";mPropertyWrite("CRC","type",type);
+    type="USB";mPropertyWrite("CRC","type",type,32);
     printf("type=%s,crc=0x%x\n",type,mCRC(data));
     
-    type="IBM";mPropertyWrite("CRC","type",type);
+    type="MODEBUS";mPropertyWrite("CRC","type",type,32);
     printf("type=%s,crc=0x%x\n",type,mCRC(data));
     
-    type="MAXIM-16";mPropertyWrite("CRC","type",type);
+    type="DNP";mPropertyWrite("CRC","type",type,32);
     printf("type=%s,crc=0x%x\n",type,mCRC(data));
     
-    type="USB";mPropertyWrite("CRC","type",type);
+    type="CRC32";mPropertyWrite("CRC","type",type,32);
     printf("type=%s,crc=0x%x\n",type,mCRC(data));
     
-    type="MODEBUS";mPropertyWrite("CRC","type",type);
-    printf("type=%s,crc=0x%x\n",type,mCRC(data));
-    
-    type="DNP";mPropertyWrite("CRC","type",type);
-    printf("type=%s,crc=0x%x\n",type,mCRC(data));
-    
-    type="CRC32";mPropertyWrite("CRC","type",type);
-    printf("type=%s,crc=0x%x\n",type,mCRC(data));
-    
-    type="MPEG2";mPropertyWrite("CRC","type",type);
+    type="MPEG2";mPropertyWrite("CRC","type",type,32);
     printf("type=%s,crc=0x%x\n",type,mCRC(data));
     
     return 0;
