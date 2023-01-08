98e8f4fbf30d5774a8431c59775da281c100e3a0
jingweizhanghuai <709894980@qq.com>
2023-01-08 14:31:43 +0800

    Update morn_ptc.h

--- include/morn_ptc.h
+++ include/morn_ptc.h
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2019-2020 JingWeiZhangHuai <jingweizhanghuai@163.com>
+Copyright (C) 2019-2023 JingWeiZhangHuai <jingweizhanghuai@163.com>
 Licensed under the Apache License, Version 2.0; you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 */
 
@@ -10,15 +10,16 @@ Licensed under the Apache License, Version 2.0; you may not use this file except
 #if defined __GNUC__
 #include <pthread.h>
 #elif defined _MSC_VER
-#include <windows.h>
 #include <process.h>
 #else
 #include <stdatomic.h>
 #include <threads.h>
 #endif
 
-#ifdef __linux
+#ifdef LINUX
 #include <ucontext.h>
+#elif defined WINDOWS
+#include <windows.h>
 #endif
 
 #ifdef __cplusplus
